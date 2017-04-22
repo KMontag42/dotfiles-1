82c649fb9e4df22175e86f74819fa62207c343eb
Kyle Montag <thekylemontag@gmail.com>
2017-04-22 17:26:02 -0400

    remove nvm files, update nvm script to install if missing

--- zsh/configs/post/nvm.zsh
+++ zsh/configs/post/nvm.zsh
@@ -1,3 +1,7 @@
 # Setup NVM
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+    . "$NVM_DIR/nvm.sh" # This loads nvm
+else
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+fi
