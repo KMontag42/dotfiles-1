0c931f5578568dd7d8099aaf43f4d486051a1095
Kyle Montag <kmontag@sessionm.com>
2017-08-17 15:25:13 -0400

    make the core temp substr a bit bigger

--- config/i3/status_command.sh
+++ config/i3/status_command.sh
@@ -3,7 +3,7 @@
 i3status | while :
 do
     read line
-    temperature=`sensors | awk 'NR==3' | awk '{ print substr($0, 18, 6) }'`
+    temperature=`sensors | awk 'NR==3' | awk '{ print substr($0, 17, 7) }'`
     clock=`date`
     cpu=`grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}'`
     current_window=`xdotool getactivewindow getwindowname`
